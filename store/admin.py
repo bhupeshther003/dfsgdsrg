cecaf9ca22a17be7f0ab126fcfd6b60a2e171c38
bhupeshther003 <bhupeshther003@gmail.com>
2023-10-11 15:48:44 +0530

    project

---
+++ store/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models.product import Products
+from .models.category import Category
+from .models.customer import Customer
+from .models.orders import Order
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+# Register your models here.
+admin.site.register(Products,AdminProduct)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+
+
+
